875d06c5e12927886a2a3d828bff1e5d19d7ff8f
fabriciosouza21 <fabriciosouza21777@gmail.com>
2021-07-08 16:43:10 -0300

    first commit

---
+++ src/main/java/com/fsm/workshopmongo/FirstSpirngbootMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.fsm.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpirngbootMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpirngbootMongodbApplication.class, args);
+	}
+
+}
