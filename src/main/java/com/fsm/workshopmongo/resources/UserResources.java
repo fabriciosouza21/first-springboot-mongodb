374c74fe8fc6335c37cb8cb72f02109b733ba0ef
fabriciosouza21 <fabriciosouza21777@gmail.com>
2021-07-10 15:55:53 -0300

    Getting a user by id

--- src/main/java/com/fsm/workshopmongo/resources/UserResources.java
+++ src/main/java/com/fsm/workshopmongo/resources/UserResources.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,7 @@ import com.fsm.workshopmongo.services.UserServices;
 @RestController
 @RequestMapping(value = "/users")
 public class UserResources {
+	
 	@Autowired 
 	private UserServices service;
 	
@@ -27,5 +29,13 @@ public class UserResources {
 		list = service.findAll();
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id){
+		
+		User user = service.findById(id);
+		
+		return ResponseEntity.ok().body(new UserDTO(user));
 	}
 }
--- src/main/java/com/fsm/workshopmongo/services/UserServices.java
+++ src/main/java/com/fsm/workshopmongo/services/UserServices.java
@@ -1,12 +1,14 @@
 package com.fsm.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.fsm.workshopmongo.entities.User;
 import com.fsm.workshopmongo.repositories.UserRepository;
+import com.fsm.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserServices {
@@ -16,5 +18,13 @@ public class UserServices {
 	
 	public List<User> findAll(){
 		return repository.findAll();
+	}
+	
+	public User findById(String id){
+		Optional<User> user = repository.findById(id);
+		if(user.isEmpty()) {
+			throw new ObjectNotFoundException("object not found");
+		}
+		return user.get();		
 	}
 }
